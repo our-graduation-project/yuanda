e449aec68209f42386ed1b2f23416d6ccba9af85
ruintu <2454714424@qq.com>
2019-12-21 11:48:40 +0800

    收藏夹接口编写

---
+++ src/main/java/wang/haogui/yuanda/service/FavoritesService.java
@@ -0,0 +1,39 @@
+package wang.haogui.yuanda.service;
+
+import wang.haogui.yuanda.model.Favorites;
+
+import java.util.List;
+
+/**
+ * @Author: Zxw
+ * @Description:
+ * @Date: Created in 11:39 2019/12/21
+ * @Modifued By:
+ */
+public interface FavoritesService {
+
+    /**
+     * 新建一个收藏夹
+     * @param favorites
+     * @return
+     */
+    boolean addFavorites(Favorites favorites);
+
+
+    /**
+     * 删除一个收藏夹
+     * @param favoritesId
+     * @return
+     */
+    boolean delFavorties(int favoritesId);
+
+
+    /**
+     * 查询一个用户的所有收藏夹
+     * @param userId
+     * @return
+     */
+    List<Favorites> selectFavoritesByUserId(int userId);
+
+
+}
