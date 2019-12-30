a77d866ef76ae4884bb4edb0e9ae125c5fa3d82b
ruintu <2454714424@qq.com>
2019-12-30 15:32:03 +0800

    收藏夹js文件

---
+++ src/main/resources/static/mydist/js/favorites-detail.js
@@ -0,0 +1,156 @@
+function modal() {
+    $('.ui.modal').modal('show');
+}
+
+var vmDetail = new Vue({
+    el:'#app',
+    created(){
+        function getQueryStringChinese(id) {
+            var reg = new RegExp("(^|&)" + id + "=([^&]*)(&|$)", "i");
+            var r = window.location.search.substr(1).match(reg);
+            if (r != null) return decodeURI(r[2]);
+            return null;
+        }
+        var id = getQueryStringChinese("id");
+        if(isNull()){
+            alert("加载收藏夹是发生错误！")
+        }else{
+            this.getData(id);
+        }
+
+    },
+    data:{
+        favorites:{},
+        favoritesName:"",
+        favoritesConnections:[]
+    },
+    methods: {
+        getData(id) {
+            let data = {"favoritesId": id};
+            $.ajax({
+                //请求方式.
+                type: "POST",
+                //请求的媒体类型
+                contentType: "application/json;charset=UTF-8",
+                //请求地址
+                url: "http://localhost:8080/yuanda/user/searchfavoritdetailbyid",
+                //数据，json字符串
+                data: JSON.stringify(data),
+                success: function (result) {
+                    vmDetail.favorites = result.data.favorites;
+                    vmDetail.favoritesName = vmDetail.favorites.favoritesName;
+                    vmDetail.favoritesConnections = result.data.favoritesConnections;
+                },
+                error: function () {
+                    alert("加载收藏夹是发生错误！")
+                }
+
+            });
+        },
+        delFavorites(){
+            let data = {"favoritesId": vmDetail.favorites.favoritesId};
+            if(isNull(vmDetail.favorites.favoritesId)){
+                alert("删除收藏夹失败");
+            }else{
+                var str = "你确定要删除标题为:"+vmDetail.favorites.favoritesName+"的收藏夹吗？";
+                if (confirm(str)) {
+                    $.ajax({
+                        //请求方式.
+                        type: "POST",
+                        //请求的媒体类型
+                        contentType: "application/json;charset=UTF-8",
+                        //请求地址
+                        url: "http://localhost:8080/yuanda/user/delfavorites",
+                        //数据，json字符串
+                        data: JSON.stringify(data),
+                        success: function (result) {
+                            alert(result.message);
+                            window.location.href = "favorites.html";
+                        },
+                        error: function () {
+                            alert("删除收藏夹失败");
+                        }
+                    });
+                }
+
+            }
+        },
+        delFavoritesConnection(index){
+                let data = {"id": vmDetail.favoritesConnections[index].id,
+                            "favoritesId": vmDetail.favorites.favoritesId,
+                            "favoritesNumber": vmDetail.favorites.favoritesNumber,
+                           };
+                var str = "你确定要删除标题为:" + vmDetail.favoritesConnections[index].contentTitle + "的收藏记录吗？";
+                if(isNull(vmDetail.favoritesConnections[index].id)){
+                    alert("删除收藏记录失败！");
+                }else if(isNull(vmDetail.favorites.favoritesId)){
+                    alert("删除收藏记录失败！");
+                }else if(isNull(vmDetail.favorites.favoritesNumber)){
+                    alert("删除收藏记录失败！");
+                }
+                else {
+                    if (confirm(str)) {
+                        $.ajax({
+                            //请求方式.
+                            type: "POST",
+                            //请求的媒体类型
+                            contentType: "application/json;charset=UTF-8",
+                            //请求地址
+                            url: "http://localhost:8080/yuanda/user/delfavoritesconnection",
+                            //数据，json字符串
+                            data: JSON.stringify(data),
+                            success: function (result) {
+                                alert(result.message);
+                                window.location.reload();
+                            },
+                            error: function () {
+                                alert("删除收藏记录失败！");
+                            }
+                        });
+                    }
+                }
+
+            }
+        },
+});
+
+function editFavorites() {
+    let editfavoritesName = $("#editfavoritesName").val();
+    let data={"editfavoritesName":editfavoritesName,
+        "favoritesId": vmDetail.favorites.favoritesId,
+    };
+
+    if(isNull(editfavoritesName)){
+        alert("编辑收藏夹失败");
+    }else if(isNull(vmDetail.favorites.favoritesId)){
+        alert("编辑收藏夹失败");
+    } else{
+        $.ajax({
+            //请求方式
+            type : "POST",
+            //请求的媒体类型
+            contentType: "application/json;charset=UTF-8",
+            //请求地址
+            url : "http://localhost:8080/yuanda/user/editfavorites",
+            //数据，json字符串
+            data : JSON.stringify(data),
+            success: function (result) {
+                if (result.result) {
+                    alert("添加成功");
+                    window.location.reload();
+                }
+                else {
+                    $("#error").html(result.message);
+                    console.log(result.message+"--"+result.result);
+                    alert("添加失败");
+                }
+            },
+            error: function () {
+                alert("编辑收藏夹失败");
+            }
+
+        });
+    }
+
+
+}
---
+++ src/main/resources/static/mydist/js/favorites.js
@@ -0,0 +1,68 @@
+function modal() {
+    $('.ui.modal').modal('show');
+}
+
+var vm = new Vue({
+    el:'#app',
+    created(){
+        this.getData();
+    },
+    data:{
+        favorites:[],
+        addFavoritesName:"",
+    },
+    methods:{
+        toDetail(index){
+            let id =vm.favorites[index].favoritesId;
+            window.location.href="favorites-detail.html?id="+id;
+        },
+        getData(){
+            axios.get('http://localhost:8080/yuanda/user/loadallfavorites').then(
+                res=>{//sucesses
+                    this.favorites = res.data.data;
+                },err=>{//error
+                    console.log(err.message);
+                });
+        }
+
+
+    }
+});
+
+
+function addFavorites() {
+    let addFavoritesName = $("#addFavoritesName").val();
+    if(isNull(addFavoritesName)){
+        alert("增加收藏夹失败，收藏夹名字不能为空！");
+    }else{
+        let data={"addFavoritesName":addFavoritesName};
+        $.ajax({
+            //请求方式
+            type : "POST",
+            //请求的媒体类型
+            contentType: "application/json;charset=UTF-8",
+            //请求地址
+            url : "http://localhost:8080/yuanda/user/addfavorites",
+            //数据，json字符串
+            data : JSON.stringify(data),
+            success: function (result) {
+                if (result.result) {
+                    alert("添加成功");
+                    window.location.reload();
+                }
+                else {
+                    $("#error").html(result.message);
+                    console.log(result.message+"--"+result.result);
+                    alert("添加失败");
+                }
+            },
+            error: function () {
+                alert("操作失败");
+            }
+
+        });
+    }
+
+}
+
+
